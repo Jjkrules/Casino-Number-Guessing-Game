e51cdf57612368090ddacf13570b711ff8bcefee
Jeswin James <tul47834@temple.edu>
2026-04-06 20:55:12 -0400

    made basic game

--- game.cpp
+++ game.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include "game.h"
 
 int main() {
-      int age;
-    std::cout << "Enter your age: ";
-    std::cin >> age; 
+    std::srand(std::time(0));  // 🔥 This fixes your issue
 
-    std::cout << "your age in years: " << age << std::endl;
+    int guess;
+    std::cout << "Enter your guess (0-10): ";
+    std::cin >> guess; 
+
+    int secretNumber = std::rand() % (10 - 0 + 1) + 0;
+
+    std::cout << "your guess: " << guess << std::endl;
+    std::cout << "secret number: " << secretNumber << std::endl;
+
+    if (guess == secretNumber) {
+        std::cout << "Congratulations! You guessed the number." << std::endl;
+    } else {
+        std::cout << "Sorry, you didn't guess the number." << std::endl;
+    }
+
     return 0;
 }
