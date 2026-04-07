a6f4f6793f255f504cbea15d9e27bda06d13f150
Jeswin James <tul47834@temple.edu>
2026-04-07 19:09:05 -0400

    finished game

--- game.cpp
+++ game.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
+#include "game.h"
 
 void showBalance(int balance) {
     std::cout << "Current Balance: $" << balance << std::endl;
 }
 
-int getbet(int balance) {
+int getBet(int balance) {
     int bet;
     std::cout << "Enter your bet amount: ";
     std::cin >> bet;
@@ -30,24 +30,42 @@ int guessNumber() {
     return guess;
 }
 
-int main() {
-    std::srand(std::time(0));  // 🔥 This fixes your issue
-    int balance = 100;
-    char chice;
-    std::cout << "Welcome to the Guessing Game!" << std::endl;
-    showBalance(balance);
-    int bet = getbet(balance);
+void playGame(int& balance) {
+    int guess = guessNumber();
+    int bet = getBet(balance);
+    int secretNumber = std::rand() % (10 - 0 + 1) + 0;
 
-   int gess = guessNumber();
-  int secretNumber = std::rand() % (10 - 0 + 1) + 0;
-    std::cout << "your guess: " << gess << std::endl;
-    std::cout << "secret number: " << secretNumber << std::endl;
+  
+    std::cout << "Your guess: " << guess << std::endl;
+    std::cout << "Secret number: " << secretNumber << std::endl;
 
-    if (gess == secretNumber) {
+    if (guess == secretNumber) {
         std::cout << "Congratulations! You guessed the number." << std::endl;
+        balance += bet; // Player wins the bet amount
     } else {
         std::cout << "Sorry, you didn't guess the number." << std::endl;
+        balance -= bet; // Player loses the bet amount
     }
+
+    showBalance(balance);
+}
+
+int main() {
+
+    std::srand(std::time(0));  
+     int balance = 100;
+    char chice;
+    std::cout << "Welcome to the Casino Number Guessing Game!" << std::endl;
+    while( balance > 0) {
+        playGame(balance);
+        std::cout << "Do you want to play again? (y/n): ";
+        std::cin >> chice;
+        if (chice != 'y' && chice != 'Y') {
+            break;
+        }
+    }
+ 
+    std::cout << "Game over. Final balance: $" << balance << std::endl;
 
     return 0;
 }
--- game.h
+++ game.h
@@ -1,4 +1,10 @@
-
+#ifndef GAME_H
 #define GAME_H
 
 void showBalance(int balance);
+int getBet(int balance);
+int getGuess();
+int generateRandomNumber(int min, int max);
+void playRound(int& balance);
+
+#endif
