d3af281e52e19c9ce36debbdf9f1ba412df8e220
Jeswin James <tul47834@temple.edu>
2026-04-06 20:44:05 -0400

    started main funtion

---
+++ game.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "game.h"
+
+int main() {
+      int age;
+    std::cout << "Enter your age: ";
+    std::cin >> age; 
+
+    std::cout << "your age in years: " << age << std::endl;
+    return 0;
+}
